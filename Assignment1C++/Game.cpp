65f3f5b16f87a87c51867b808632981ad0f42463
dhruvverma <verdy016@mymail.unisa.edu.au>
2024-03-26 21:25:46 +1030

    Write the methods for the game class

--- Assignment1C++/Game.cpp
+++ Assignment1C++/Game.cpp
@@ -4,6 +4,12 @@
 #include "RandomGenerator.h"
 
 class Game {
+	enum class GamePhase {
+		Orbiting,
+		Landed,
+		Selling
+	};
+
 private:
 	int cargoValue;
 	int balance;
@@ -14,8 +20,9 @@ private:
 	ItemManager itemManager;
 	MoonManager moonManager;
 	RandomGenerator randomGenerator;
+	GamePhase phase;
+	std::vector<Employee> employees;
 
-	// Items[]
 
 public:
 	Game(int balance, int quota) :
@@ -27,9 +34,26 @@ public:
 		remainingEmployees(0),
 		itemManager(),
 		moonManager(),
-		randomGenerator() {}
+		randomGenerator(),
+		phase(GamePhase::Orbiting) {}
 
 	void initialiseGame() {
 
 	}
+
+	int define_moons() {}
+	int define_items() {}
+	int show_welcome_screen() {}
+	int run_game() {}
+	int run_day_loop() {}
+	int read_and_dispatch_commands() {}
+	int handle_land_command() {}
+	int handle_leave_command() {}
+	int handle_exit_command() {}
+	int update_balance(int amount) {}
+	int update_current_moon(std::string moon_name) {}
+	int update_game_phase(GamePhase phase) {}
+	int update_cargo_value(int amount) {}
+	int update_alive_employees(int count) {}
+
 };
