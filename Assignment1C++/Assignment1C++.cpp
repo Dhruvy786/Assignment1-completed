cd22af7cf48570cf97233d129423225b67756a1f
dhruvverma <verdy016@mymail.unisa.edu.au>
2024-03-26 11:06:55 +1030

    Add project files.

---
+++ Assignment1C++/Assignment1C++.cpp
@@ -0,0 +1,12 @@
+﻿// Assignment1C++.cpp : Defines the entry point for the application.
+//
+
+#include "Assignment1C++.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
