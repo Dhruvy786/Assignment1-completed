b5d162c549539f68e95211af2b0e7e257d722fca
dhruvverma <verdy016@mymail.unisa.edu.au>
2024-03-30 00:24:08 +1030

    Made display_welcome_screen and read_and_dispatch_commands methods

--- Assignment1C++/Game.cpp
+++ Assignment1C++/Game.cpp
@@ -48,12 +48,54 @@ public:
 		phase = GamePhase::Orbiting;
 	}
 
-	int define_moons() {}
+	int define_moons() {
+
+	}
 	int define_items() {}
-	int show_welcome_screen() {}
+
+	int show_welcome_screen() {
+		std::cout << "    ___               _ _           ___                 \n" \
+					 "   /   \\___  __ _  __| | |_   _    / __\\___  _ __ _ __  \n" \
+					 "  / /\\ / _ \\/ _` |/ _` | | | | |  / /  / _ \\| '__| '_ \\ \n" \
+					 " / /_//  __/ (_| | (_| | | |_| | / /__| (_) | |  | |_) |\n" \
+					 "/___,' \\___|\\__,_|\\__,_|_|\\__, | \\____/\\___/|_|  | .__/ \n" \
+					 "                          |___/                  |_|    " << std::endl;
+		std::cout << "Welcome!\n"
+					 "We trust you will be a great asset to the corporation!\n\n"
+					 "============ = DAY 1 ============ =\n"
+					 "Current cargo value : $0\n"
+					 "Current balance : $50\n"
+					 "Current quota : $150(3 days left to meet quota)\n"
+					 "Currently orbiting : Corporation\n\n"
+					 "> MOONS\n"
+					 "To see the list of moons the autopilot can route to.\n\n"
+					 "> STORE\n"
+					 "To see the company store's selection of useful items.\n\n"
+					 "> INVENTORY\n"
+					 "To see the list of items you've already bought.\n\n" 
+					 "> " << std::endl;
+
+		return 0;
+	}
+
 	int run_game() {}
 	int run_day_loop() {}
-	int read_and_dispatch_commands() {}
+	std::string read_and_dispatch_commands() {
+		std::string command;
+
+		while (true) {
+			std::cout << "Enter a command: ";
+			std::cin >> command;
+
+			if (command == "LAND" || command == "LEAVE" || command == "EXIT") {
+				return command;
+			}
+			else {
+				std::cout << "Invalid command. Please enter a valid command.\n" << std::endl;
+			}
+		}
+	}
+
 	int handle_land_command() {}
 	int handle_leave_command() {}
 	int handle_exit_command() {}
