6c4308ccf8d25f0359abe1eca4bcac430f660842
dhruvverma <verdy016@mymail.unisa.edu.au>
2024-04-12 11:38:43 +0930

    Did comments for AbstractMoon class and Item class

--- Assignment1C++/AbstractMoon.cpp
+++ Assignment1C++/AbstractMoon.cpp
@@ -8,14 +8,17 @@ AbstractMoon::AbstractMoon(std::string moonName, float minScrapValue, float maxS
     baseSurvivalChance(baseSurvivalChance),
     weather(MoonWeather::Clear) {}
 
+// Get the name of the moon
 const std::string& AbstractMoon::name() const {
     return moonName;
 }
 
+// Get the weather condition of the moon
 AbstractMoon::MoonWeather AbstractMoon::getWeather() const {
     return weather;
 }
 
+// Set the weather condition of the moon based on the given number
 void AbstractMoon::setWeather(int num) {
     if (num == 0) {
         this->weather = MoonWeather::Clear;
@@ -31,18 +34,22 @@ void AbstractMoon::setWeather(int num) {
     }
 }
 
+// Get the minimum scrap value of the moon
 float AbstractMoon::getMinScrapValue() {
     return minScrapValue;
 }
 
+// Get the maximum scrap value of the moon
 float AbstractMoon::getMaxScrapValue() {
     return maxScrapValue;
 }
 
+// Get the base survival chance of the moon
 float AbstractMoon::getBsc() {
     return baseSurvivalChance;
 }
 
+// Get the multiplier values for different weather conditions
 std::vector<float> AbstractMoon::getMultiplierValue() {
     std::vector<float> weatherConditions = { 1, 1, 1 };
     if (this->weather == MoonWeather::Flooded) {
--- Assignment1C++/Item.cpp
+++ Assignment1C++/Item.cpp
@@ -1,41 +1,48 @@
 #include <iostream>
 #include "Item.h"
 
-
+// Constructor to initialize the Item object with provided attributes
 Item::Item(std::string name, int price, float svm, float escm, float oscm, float esc, float lrm) {
-	this->name = name;
-	this->price = price;
-	this->scrapValueMultiplier = svm;
-	this->explorerSurvivalChanceMultiplier = escm;
-	this->operatorSurvivalChanceMultiplier = oscm;
-	this->explorerSaveChance = esc;
-	this->lootRecoveryMultiplier = lrm;
+    this->name = name;
+    this->price = price;
+    this->scrapValueMultiplier = svm;
+    this->explorerSurvivalChanceMultiplier = escm;
+    this->operatorSurvivalChanceMultiplier = oscm;
+    this->explorerSaveChance = esc;
+    this->lootRecoveryMultiplier = lrm;
 }
 
+// Get the name of the item
 std::string Item::getName() {
-	return name;
+    return name;
 }
 
+// Get the price of the item
 int Item::getPrice() {
-	return price;
+    return price;
 }
 
+// Get the scrap value multiplier of the item
 float Item::getSvm() {
-	return scrapValueMultiplier;
+    return scrapValueMultiplier;
 }
 
+// Get the explorer survival chance multiplier of the item
 float Item::getEscm() {
-	return explorerSurvivalChanceMultiplier;
+    return explorerSurvivalChanceMultiplier;
 }
 
+// Get the operator survival chance multiplier of the item
 float Item::getOscm() {
-	return operatorSurvivalChanceMultiplier;
+    return operatorSurvivalChanceMultiplier;
 }
 
+// Get the explorer save chance of the item
 float Item::getEsc() {
-	return explorerSaveChance;
+    return explorerSaveChance;
 }
 
+// Get the loot recovery multiplier of the item
 float Item::getLrm() {
-	return lootRecoveryMultiplier;
+    return lootRecoveryMultiplier;
 }
